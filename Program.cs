b2f56d5a959c4c16daa5cbf71b6795c616601b93
dbjdbj <dbj@dbj.org>
2023-10-12 14:09:08 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ internal class Program
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
-                break;
+                // break;
             }
         }
     }
